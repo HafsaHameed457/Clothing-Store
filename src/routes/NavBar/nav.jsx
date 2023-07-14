df1d1ee65961e516a1c30b771d9aaeda4d110b96
Hafsa Hameed <hafsa.hameed457@gmail.com>
2023-07-14 14:20:39 +0500

    Home Page and NavBar styled

---
+++ src/routes/NavBar/nav.jsx
@@ -0,0 +1,25 @@
+import { ReactComponent as Logo } from "../../007 crown.svg";
+import React from "react";
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import "./nav.styles.scss";
+
+const Nav = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-links" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Nav;
---
+++ src/routes/home/home.jsx
@@ -0,0 +1,37 @@
+import CategoryDesk from "../../components/category-desk/category-desk";
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+    imgUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    imgUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    imgUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "Womens",
+    imgUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 4,
+    title: "Mens",
+    imgUrl: "https://i.ibb.co/R70vBrQ/womens.png",
+  },
+];
+const Home = () => {
+  return (
+    <div>
+      <CategoryDesk categories={categories} />
+    </div>
+  );
+};
+
+export default Home;
